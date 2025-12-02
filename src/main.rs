4e7bf65596015fa8b53ae96c47e313d9c0a5fbd6
Mario Macias <mario.macias@grafana.com>
2025-12-02 13:31:54 +0100

    Basic graph macro structure

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,66 @@ use tokio_util::sync::CancellationToken;
 
 macro_rules! graph {
     {
-        nodes: {
-            $node_name:ident : $node_func:path
+        extract {
+            $($enode_name:ident : $enode_func:path,)*
         }
+        transform {
+            $($tnode_name:ident : $tnode_func:path,)*
+        }
+        load {
+            $($lnode_name:ident : $lnode_func:path,)*
+        }
+        $($src_name:ident -> $dst_name:path,)*
     } => {
-        println!("Instantiating {}: {}", stringify!($node_name), stringify!($node_func));
+        fn check_enode_signature<T: Send + 'static>(
+            _f: impl Fn(CancellationToken, tokio::sync::mpsc::Sender<T>) -> std::pin::Pin<Box<dyn std::future::Future<Output = ()> + Send>>
+        ) {}
+
+        let ct = CancellationToken::new();
+        // Validate extract node signatures at compile time
+        $(
+            const _: fn() = || {
+                // This will fail to compile if $enode_func doesn't match the expected signature
+                check_enode_signature(|ct, out| Box::pin($enode_func(ct, out)));
+            };
+        )*
+        // TODO: validate tnode and lnode signatures
+
+        let mut channels = HashMap::new();
+        $({
+            // TODO: optimize (e.g. use only MPSC when multiple submitters are detected)
+            let (rx, tx) = tokio::sync::mpsc::channel(100);
+            channels.insert($tnode_name, tx);
+        })*
+        $({
+            // TODO: optimize (e.g. use only MPSC when multiple submitters are detected)
+            let (rx, tx) = tokio::sync::mpsc::channel(100);
+            channels.insert($lnode_name, tx);
+        })*
+
+
+
+        // let ct = CancellationToken::new();
     };
 }
 
 #[tokio::main]
 async fn main() {
     let a = 33;
-    graph!{
-        nodes: { procs: tracers::proc::trace_processes }
+    graph! {
+        extract {
+            procs:       tracers::proc::trace_processes,
+        }
+        transform {
+            proc_renamer: transform::proc_renamer,
+        }
+        load {
+            otel_export: export::otel::exporter,
+        }
+        procs -> proc_renamer,
+        proc_renamer -> otel_export,
     };
+
     let ct = CancellationToken::new();
 
     let (procs_output, transform_input) = tokio::sync::mpsc::channel(100);
